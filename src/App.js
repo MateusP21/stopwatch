d046520db18bfe2432bbe7ef50b273da7fdef8d8
MateusP21 <mateuspasso@outlook.com>
2022-03-06 14:45:41 -0300

    componente Display criado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
+import Display from "./components/Display/Display";
 
 function App() {
   return (
     <div className="App">
-
+      <Display/>
     </div>
   );
 }
---
+++ src/components/Display/Display.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.css'
+
+export default class Display extends React.Component {
+  render() {
+    return <h1>Display</h1>
+  }
+}
