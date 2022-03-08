01d20e09a4bbabcfe31f7ed6dd9f679578be70d2
MateusP21 <mateuspasso@outlook.com>
2022-03-08 16:38:06 -0300

    fix operador

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -5,8 +5,8 @@ export default class Display extends React.Component {
   constructor() {
     super();
     this.state = {
-      startingMinutes: 0,
-      startingSeconds: 0,
+      startingMinutes: '0',
+      startingSeconds: '0',
       time: 0,
       seconds: 0,
       minutes: 0,
@@ -61,7 +61,7 @@ export default class Display extends React.Component {
 
   startCountdown = () => {
     const { startingMinutes, startingSeconds } = this.state;
-    if (startingMinutes === 0 || startingSeconds === 0) {
+    if (startingMinutes === 0 && startingSeconds === 0) {
       toast.error('Valores inválidos 😔', {
         position: 'top-center',
         autoClose: 5000,
